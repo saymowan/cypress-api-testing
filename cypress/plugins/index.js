b2d61a01b0ff455945fb8b66b12d7717638f389f
diegofarias1 <diego.farias@base2.com.br>
2021-03-12 15:57:13 -0300

    Add Multi-environments-and-conection-db-example

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,21 +1,54 @@
 /// <reference types="cypress" />
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
 
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
-
-/**
- * @type {Cypress.PluginConfig}
- */
-module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-}
+ const mysql = require('mysql')
+ // Dados da conexão
+ const connections = {
+   bugtracker: {
+     host: 'localhost',
+     user: 'root',
+     password: 'root',
+     database: 'bugtracker'
+   }
+ }
+ function queryDB (connectionInfo, query) {
+   const connection = mysql.createConnection(connectionInfo)
+ 
+   connection.connect()
+ 
+   return new Promise((resolve, reject) => {
+     connection.query(query, (error, results) => {
+       if (error) {
+         return reject(error)
+       }
+ 
+       connection.end()
+ 
+       return resolve(results)
+     })
+   })
+ }
+ module.exports = (on, config) => {
+   on('task', {
+     // destructure the argument into the individual fields
+     queryDatabase ({ dbName, query }) {
+       const connectionInfo = connections[dbName]
+ 
+       if (!connectionInfo) {
+         throw new Error(`Do not have DB connection under name ${dbName}`)
+       }
+       return queryDB(connectionInfo, query)
+ 
+     },
+   })
+   //Environments config
+   const fs = require('fs-extra')
+   const path = require('path')
+   function getConfigurationByFile (file) {
+     // caminho da pasta onde estão presentes os arquivos JSON dos ambientes
+     const pathToConfigFile = path.resolve('environmentsConfig', `${file}.json`)
+   
+     return fs.readJson(pathToConfigFile)
+   }
+   const file = config.env.configFile || 'ExampleQa'
+   return getConfigurationByFile(file)
+ }
